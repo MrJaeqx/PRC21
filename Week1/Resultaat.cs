2bc8fc7a1f1ae0ff911c34423d311803ab08abf0
MrJaeqx <lars.jaeqx@gmail.com>
2014-02-05 09:22:07 +0100

    Opgave 4

---
+++ Week1/Resultaat.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Week1
+{
+    class Resultaat
+    {
+
+        private Vak vak;
+        private int cijfer;
+
+        Resultaat(Vak vak, int cijfer) {
+            this.vak = vak;
+            this.cijfer = cijfer;
+        }
+
+        public Vak getVak()
+        {
+            return this.vak;
+        }
+
+        public int getCijfer()
+        {
+            return this.cijfer;
+        }
+    }
+}
