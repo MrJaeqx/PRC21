70abcc89fa6e7958ff398501d3dd452ab169bbd4
Wouter Habets <wouterhabets@gmail.com>
2014-02-05 10:04:10 +0100

    ListTools

---
+++ Week1/ListTools.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Week1
+{
+    class ListTools
+    {
+
+        public int GetMax(List<int> list)
+        {
+            int max = 0;
+            foreach (int nummer in list)
+            {
+                if (nummer > max) max = nummer;
+            }
+            return max;
+        }
+
+        public int GetMin(List<int> list)
+        {
+            int min = 10;
+            foreach (int nummer in list)
+            {
+                if (nummer < min) min = nummer;
+            }
+            return min;
+        }
+    }
+}
