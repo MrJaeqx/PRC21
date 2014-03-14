f7890084327bcfce1154541be71ad7af0e3f50e5
MrJaeqx <lars.jaeqx@gmail.com>
2014-03-14 10:26:47 +0100

    TestUnit

---
+++ AnimalShelter/TestProject/CatTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AnimalShelter;
+
+namespace TestProject
+{
+    [TestClass]
+    public class CatTest
+    {
+        [TestMethod]
+        public void TestBadHabitsSet()
+        {
+            // arrange
+            string badHabit = "Hoi";
+            string expected = "Hoi";
+            Cat cat = new Cat("1337", new SimpleDate(1,1,1), "Naam", "", false);
+
+            // act
+            cat.BadHabits = badHabit;
+
+            // assert
+            string actual = cat.BadHabits;
+            Assert.AreEqual(expected, actual, "Bad habit not set");
+        }
+
+        [TestMethod]
+        public void TestReservedSet()
+        {
+            // arrange
+            bool reserved = true;
+            bool expected = true;
+            Cat cat = new Cat("1337", new SimpleDate(1, 1, 1), "Naam", "", false);
+
+            // act
+            cat.IsReserved = reserved;
+
+            // assert
+            bool actual = cat.IsReserved;
+            Assert.AreEqual(expected, actual, "Reserved not set");
+        }
+    }
+}
---
+++ AnimalShelter/TestProject/DogeTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AnimalShelter;
+
+namespace TestProject
+{
+    [TestClass]
+    public class DogeTest
+    {
+        [TestMethod]
+        public void TestReservedSet()
+        {
+            // arrange
+            bool reserved = true;
+            bool expected = true;
+            Dog dog = new Dog("1337", new SimpleDate(1, 1, 1), "Naam", new SimpleDate(1,1,1), false);
+
+            // act
+            dog.IsReserved = reserved;
+
+            // assert
+            bool actual = dog.IsReserved;
+            Assert.AreEqual(expected, actual, "Reserved not set");
+        }
+    }
+}
